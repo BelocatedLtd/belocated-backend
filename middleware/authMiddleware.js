e9390eeabaa692d31072d6caabee11c6df474bc3
codecjay <mail2jhenry@gmail.com>
2023-07-28 10:57:55 +0100

    token fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,22 +4,22 @@ import jwt from "jsonwebtoken";
 
 export const protect = asyncHandler(async (req, res, next) => {
 
-    const authToken = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    const authToken = req.headers.authorization?.split(' ')[1];
 
     if(!authToken) {
         res.status(401).json({message: "Not Authorized, no token"});
         throw new Error( 'Not Authorized, no token ')
     }
 
-        try {
-            const decoded = jwt.verify(authToken, process.env.JWT_SECRET);
+    try {
+        const decoded = jwt.verify(authToken, process.env.JWT_SECRET);
 
-            req.user = await User.findById(decoded.id).select('-password');
+        req.user = await User.findById(decoded.id).select('-password');
 
-            next();
-        } catch (error) {
-            res.status(401).json({message: "Not Authorized, invalid token"});;
-            throw new Error('Not authorized, invalid token')
-        }
+        next();
+    } catch (error) {
+        res.status(401).json({message: "Not Authorized, invalid token"});;
+        throw new Error('Not authorized, invalid token')
+    }
 })
 
