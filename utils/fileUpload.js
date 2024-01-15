691d13927e43804d5f1ed418b78aac7c4f187fa6
nkenna <nkennannadi@gmail.com>
2024-01-15 18:57:10 +0100

    ghhh

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -15,7 +15,8 @@ const storage = multer.diskStorage({
     if (
         file.mimetype === 'image/png' ||
         file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg'
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'video/mp4'
     ) {
         cb(null, true)
     } else {
