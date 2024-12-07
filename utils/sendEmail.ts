9c430e31d71a3461449e914d2f09ee884360daf9
BelocatedLtd <belocatedltd@gmail.com>
2024-12-07 23:07:27 +0100

    Update sendEmail.ts spaming issue

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,64 +1,42 @@
-import nodemailer from 'nodemailer'
-// import Mailgun from 'mailgun.js';
-import dotenv from 'dotenv'
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-//import mg from 'nodemailer-mailgun-transport'
-
-// const API_KEY = process.env.MAILGUN_API_KEY;
-// const DOMAIN = 'mail.belocated.ng';
-
-const HOST = process.env.EMAIL_HOST
-const USER = process.env.EMAIL_USER
-const PASS = process.env.EMAIL_PASS
-
-console.log('🚀 ~ HOST:', { HOST, USER, PASS })
+const HOST = process.env.EMAIL_HOST;
+const USER = process.env.EMAIL_USER;
+const PASS = process.env.EMAIL_PASS;
 
 var transport = nodemailer.createTransport({
-	host: HOST,
-	port: 587,
-	auth: {
-		user: USER,
-		pass: PASS,
-	},
-})
+  host: HOST,
+  port: 465,
+  secure: true, // Use SSL
+  auth: {
+    user: USER,
+    pass: PASS,
+  },
+});
 
-// const mailgun = new Mailgun(formData);
-// const client = mailgun.client({username: 'api', key: API_KEY});
+const sendEmail = async (subject: any, message: any, send_to: any, reply_to:any, plainText:any,) => {
 
-// const sendEMail = async(subject, message, send_to, reply_to)  => {
-//     const messageData = {
-//         from: 'Belocated <cs@belocated.ng>',
-//         to: send_to,
-//         subject: subject,
-//         html: message
-//       };
+  var mailOptions = {
+    from: '"Belocated" <cs@belocated.ng>',
+    to: send_to,
+    subject: subject,
+    text: plainText,
+    html: message,
+    replyTo: reply_to, // Valid domain email address
+  };
 
-//     try {
-//           const res = client.messages.create(DOMAIN, messageData)
-//           console.log(res)
-//           return res
-//     } catch (error) {
-//         console.log(error)
-//         throw new Error(error)
-//     }
-// }
+  try {
+    const res = await transport.sendMail(mailOptions);
+    console.log('Email sent successfully:', res);
+    return res;
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    throw new Error('Failed to send email. Please try again.');
+	
+  }
+};
 
-const sendEmail = async (subject: string, message: string, send_to: string, reply_to: string) => {
-	var mailOptions = {
-		from:'"Belocated" <cs@belocated.ng>',
-		to: send_to,
-		subject: subject,
-		html: message,
-	}
-
-	try {
-		const res = await transport.sendMail(mailOptions)
-		return res
-	} catch (error) {
-		throw new Error(error as string)
-	}
-}
-
-export default sendEmail
+export default sendEmail;
